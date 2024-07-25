303e0505b132466da5558f6ada0146f33d5bfa56
marco <marcochisci@edu.unifi.it>
2024-07-25 15:47:03 +0200

    Created CMake file, readME and gitignore

---
+++ cmake-build-debug/CMakeFiles/Parallel_Programming_Projects.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Parallel_Programming_Projects.dir/main.cpp.o"
+  "CMakeFiles/Parallel_Programming_Projects.dir/main.cpp.o.d"
+  "Parallel_Programming_Projects"
+  "Parallel_Programming_Projects.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Parallel_Programming_Projects.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Parallel_Programming_Projects.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Parallel_Programming_Projects.
+# This may be replaced when dependencies are built.
