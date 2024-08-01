2413dbcb083d096579684cb106913e22e81a22c0
marco <marcochisci@edu.unifi.it>
2024-08-01 18:51:05 +0200

    Sequential Kmeans

---
+++ cmake-build-debug/CMakeFiles/Kmeans_clustering.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/Marco/Desktop/Parallel-Programming-Projects/Cluster.cpp" "CMakeFiles/Kmeans_clustering.dir/Cluster.cpp.o" "gcc" "CMakeFiles/Kmeans_clustering.dir/Cluster.cpp.o.d"
+  "/mnt/c/Users/Marco/Desktop/Parallel-Programming-Projects/KMeansSeq.cpp" "CMakeFiles/Kmeans_clustering.dir/KMeansSeq.cpp.o" "gcc" "CMakeFiles/Kmeans_clustering.dir/KMeansSeq.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Kmeans_clustering.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Kmeans_clustering.
+# This may be replaced when dependencies are built.
